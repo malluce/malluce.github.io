3d02cf16df4a3687e6cde3c44ddec05ab38c4e8f
Felix Bachmann <felix.bachmann@ewetel.net>
2019-01-04 13:33:29 +0100

    added css, functions

--- ambient/ambient.js
+++ ambient/ambient.js
@@ -1,26 +1,59 @@
-if ("AmbientLightSensor" in window) { // generic sensor API
-    console.log("using AmbientLightSensor to retrieve lux values")
-    try {
-      var sensor = new AmbientLightSensor();
-      sensor.addEventListener("reading", function (event) {
-        update(sensor.illuminance);
-      });
-      sensor.start();
-    } catch (e) {
-      console.error(e);
+/**
+ * 
+ * @param {*} threshold 
+ */
+function start(threshold) {
+    function update(lux) {
+        document.getElementById("sensor-value").innerHTML = lux + " lux"
+        console.log(lux)
+        if(lux > threshold) {
+            light()
+        } else {
+            dark()
+        }
     }
-} else if ("ondevicelight" in window) { // legacy API
-    console.log("using ondevicelight to retrieve lux values")
-    function onUpdateDeviceLight(event) {
-      update(event.value);
+
+    if ("AmbientLightSensor" in window) { // generic sensor API (Chrome etc.)
+        console.log("using AmbientLightSensor to retrieve lux values")
+        try {
+          var sensor = new AmbientLightSensor();
+          sensor.addEventListener("reading", function (event) {
+            update(sensor.illuminance);
+          });
+          sensor.start();
+        } catch (e) {
+          console.error(e);
+        }
+    } else if ("ondevicelight" in window) { // legacy API (Firefox)
+        console.log("using ondevicelight to retrieve lux values")
+        function onUpdateDeviceLight(event) {
+          update(event.value);
+        }
+        
+        window.addEventListener("devicelight", onUpdateDeviceLight);
+    } else {
+        alert("Your browser is not ambient light ready! Use Firefox, Edge, or Chrome with experimental Generic Sensor API Flag and try again.")
     }
-    
-    window.addEventListener("devicelight", onUpdateDeviceLight);
-} else {
-    console.log("Your browser is not ambient light ready! Use Firefox or Chrome with experimental Generic Sensor API Flag and try again.")
 }
 
-function update(lux) {
-    document.getElementById("sensor-value").innerHTML = lux + " lux"
-    console.log(lux)
+var debug = true;
+
+function toggleDebug() {
+    debug = !debug
+
+    var debugElem = document.getElementById("debug")
+
+    if(debug) {
+        debugElem.style.visibility = "visible"
+    } else {
+        debugElem.style.visibility = "hidden"
+    }
+}
+
+function light() {
+    document.getElementById("value-class").innerHTML = "light"
+}
+
+function dark() {
+    document.getElementById("value-class").innerHTML = "dark"
 }
