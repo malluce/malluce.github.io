bd3104b658e9502d07ee48d5f073db3736a43e75
Felix Bachmann <felix.bachmann@ewetel.net>
2019-01-04 12:28:02 +0100

    changing html based on sensor value

--- ambient/ambient.js
+++ ambient/ambient.js
@@ -1,4 +1,5 @@
 if ("AmbientLightSensor" in window) { // generic sensor API
+    console.log("using AmbientLightSensor to retrieve lux values")
     try {
       var sensor = new AmbientLightSensor();
       sensor.addEventListener("reading", function (event) {
@@ -8,14 +9,18 @@ if ("AmbientLightSensor" in window) { // generic sensor API
     } catch (e) {
       console.error(e);
     }
-} else if ("ondevicelight" in window) {
+} else if ("ondevicelight" in window) { // legacy API
+    console.log("using ondevicelight to retrieve lux values")
     function onUpdateDeviceLight(event) {
       update(event.value);
     }
     
     window.addEventListener("devicelight", onUpdateDeviceLight);
+} else {
+    console.log("Your browser is not ambient light ready! Use Firefox or Chrome with experimental Generic Sensor API Flag and try again.")
 }
 
 function update(lux) {
+    document.getElementById("sensor-value").value = lux
     console.log(lux)
 }
